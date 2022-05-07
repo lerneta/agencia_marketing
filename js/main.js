1bfbe2b9bcc8462905ff6134249b1e2cf37ec347
Luciana Erneta <luciana.erneta@hotmail.com>
2022-05-07 12:04:35 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
     // Preloader (if the #preloader div exists)
     $(window).on('load', function () {
+
         if ($('#preloader').length) {
             $('#preloader').delay(100).fadeOut('slow', function () {
                 $(this).remove();
